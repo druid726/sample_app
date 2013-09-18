8b24e38d9fff89fcd17649873bc3206261b35c94
Pat O'Neill <pdoneill26@gmail.com>
2013-09-18 14:04:46 -0400

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "UserPages" do
+	subject { page }
+
+  describe "singup page" do
+  		before { visit singup_path }
+
+  		it { should have_content ('Sing up') }
+  		it { should have_title(full_title('Sign up')) }
+   end
+end
