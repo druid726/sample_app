25294c116ffe9ba583ac2dc43281e27d774b70c4
Pat O'Neill <pdoneill26@gmail.com>
2013-09-30 13:39:23 -0400

    Removed sqlite3 from Gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ SampleApp::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   #Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = ture
+  config.force_ssl = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
