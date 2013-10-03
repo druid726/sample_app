d5ea6a1e73c36a4c83d77972368b580925f4114d
Pat O'Neill <pdoneill26@gmail.com>
2013-10-02 22:10:28 -0400

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ end
   it { should respond_to (:password_digest) }
   it { should respond_to (:password) }
   it { should respond_to (:password_confirmation) }
+  it { should respond_to (:remember_token) }
   it { should respond_to (:authenticate) }
 
 	describe "when name is not present" do
@@ -91,5 +92,10 @@ describe "when email is not present" do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
+  end
+
+  describe "remember_token" do
+    before { @user.save }
+    its (:remember_token) { should_not be_blank }
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,9 +4,9 @@ describe "User pages" do
 	
 	subject { page }
 
-			it { should have_content ('Sign up') }
+			it { should have_content('Sign up') }
   		it { should have_title(full_title('Sign up')) }
-   end   
+  
 
    describe "profile page" do
 		let(:user) { FactoryGirl.create(:user) }
@@ -21,7 +21,7 @@ describe "User pages" do
 	  describe "signup page" do
   		before { visit signup_path }
 
-  		let(:submit) { "Create my account"}
+  		let(:submit) { "Create my account" }
 
   		describe "with invalid information" do
   			it "should not create a user" do
@@ -38,7 +38,17 @@ describe "User pages" do
 			end
 
 			it "should create a user" do
-				expect { click_button submit }.to change(User, :count) .by(1)
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				let(:user)  { User.find_by(email: 'user@example.com') }
+
+				it { should have_link('Sign out') }
+				it { should have_title(user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
+end
